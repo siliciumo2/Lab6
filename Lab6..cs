a819e301730867167b840d3219c5d99e732e6460
Siliciumo2 <147613900+siliciumo2@users.noreply.github.com>
2023-11-22 13:35:55 +0300

    Add files via upload

---
+++ Lab6..cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Введите количество строк и столбцов матрицы");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int[,] massiv = new int[x, y];
+Console.WriteLine("Введите элементы массива");
+for (int i = 0; i < x; i++)
+{
+    for (int j = 0; j < y; j++)
+    {
+        massiv[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+int min = massiv[0, 0];
+for (int i = 0; i < x; i++)
+{
+    for (int j = 0; j < y; j++)
+        if ((massiv[i, j] < min) &(massiv[i, j]!=0))
+        {
+            min = massiv[i, j];
+        }
+}
+for (int i = 0; i < x; i++)
+{
+    for (int j = 0; j < y; j++)
+    {
+        if (massiv[i, j] == 0)
+        {
+            massiv[i, j] = min;
+        }
+    }
+}
+Console.WriteLine("Полученный массив =");
+for (int i = 0; i < x; i++)
+{
+    for (int j = 0; j < y; j++)
+    {
+        Console.WriteLine(massiv[i, j]);
+    }
+}
